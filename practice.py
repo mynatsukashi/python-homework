88647f4f77f74983cb67156f2bc56e1dd7bfc4af
Natali Nasidze <natali.nasidze.1@gmail.com>
2026-06-20 10:03:35 +0100

    homework5

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
-def add(a, b):
-    # Print doesn't return any value, it displays it in terminal and that's it. Meanwhile, Return displays, store its value and uses whenever we call it.
-    return(a + b)
+def greet(name="World"):
+    return f"Hello, {name}!"
 
-total = add(10, 5) + 100
-print(f"{total = }")
+greet()
+
+
+
+
