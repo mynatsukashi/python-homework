95067e887d471aab1fbeb0e3dde0df45c10b451c
Natali N. <144839845+mynatsukashi@users.noreply.github.com>
2026-07-01 01:10:01 +0100

    Update exercise_8UNSOLVED.py

--- homework7/exercise_8UNSOLVED.py
+++ homework7/exercise_8UNSOLVED.py
@@ -11,7 +11,7 @@ except FileNotFoundError:
     # If program doesn't find the file, it will print this message
     print("File is not found")
 else: 
-    # If file does exist, ask user to add a note
+    # If file exists, ask user to add a note
     add_note =input("Add a note: ")
     # Adds this note in notes list
     notes.append(add_note)
@@ -20,7 +20,7 @@ finally:
     def save_note():
         for i, note in enumerate(notes, 1):
             print(f"{i}. {note}")
-            # I try to print final results in "notes.txt" file, but it doesn't work
+            # I tried to print final results in "notes.txt" file, but it doesn't work
         with open ("notes.txt","a") as f:
             f.write(save_note + "\n")
     print("Saved!")
