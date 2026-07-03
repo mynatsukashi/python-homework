f2c93d1343dedaaf5ee6f30dce5ecbf8bf7f1bc8
Natali Nasidze <natali.nasidze.1@gmail.com>
2026-07-03 22:14:01 +0100

    push random_game

--- practice/random_game.py
+++ practice/random_game.py
@@ -5,32 +5,27 @@ The concept is to guess a word. 3 attempts.
 from random import choice
 import time
 
-mistake = 0
-cities = ["tbilisi","mtskheta", "kazbegi", "telavi", "rustavi", "bolnisi", "dmanisi", "batumi", "poti", "gori", "kutaisi", "qobuleti"]
+cities = ["tbilisi","mtskheta", "kazbegi", "telavi", "rustavi", "bolnisi", "dmanisi", "batumi", "poti", "gori", "kutaisi", "kobuleti"]
 
-"""
-- Write a function
-"""
-random_city = choice(cities)
-print("Lem me choose the word...")
-# time.sleep(2)
-print("Let's go!")
+rand_city = choice(cities)
 
-def hint(random_city):
-        print(random_city[0])
+def hint():
+    hint = rand_city.title()[0] + (len(rand_city)- 1)* "_"
+    return hint
 
-lst = (len(random_city)-1) * "_"
-print(random_city[0].title(),lst)
+attempt = 3
 
-ask_question = input("What word have I chosen?").lower().strip()
+print("Choosing a word...")
+time.sleep(2)
+print(hint())
+chosen_city = input("What word have I chosen?").lower().strip()
 
-if ask_question != random_city:
-        while mistake <3:
-                print(mistake)
-                mistake +=1
-        print("Try again!")
-elif mistake ==3:
-        print("Game is over. You lost!")
+if chosen_city != rand_city:
+        while attempt>1:
+            print(f"Wrong! Number of attempts:{attempt}")
+            attempt-=1
+            again = input("Try again: ")
+        else:
+            print("Game is over. You lost!")
 else:
-        print("Congratulations, you won!")
-
+        print("You won!")
